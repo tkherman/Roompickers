45ac5e820bfcee92fc84698daa63c8599f0b2653
tkherman <ktong1@dsg1.crc.nd.edu>
2018-04-22 12:58:33 -0400

    added get_rooms and filter_rooms

--- server.py
+++ server.py
@@ -83,6 +83,82 @@ def update_student():
 
     return "Update succeeded"
 
+@app.route('/floors/<netID>/<dorm_name>')
+def get_rooms(netID, dorm_name):
+    mens_dorm = ['Alumni', 'Carroll', 'Dillon', 'Duncan', 'Dunne', 'Fisher',
+                 'Keenan', 'Keough', 'Knott', 'Morrisey', 'O\'Neil Family',
+                 'Siegfried', 'Sorin', 'St. Edward\'s', 'Stanford', 'Zahm']
+    womens_dorm = ['Badin', 'Breen-Phillips', 'Cavanaugh', 'Farley', 'Flaherty',
+                   'Howard', 'Lewis', 'Lyons', 'McGlinn', 'Pasquerilla East',
+                   'Pasquerilla West', 'Ryan', 'Walsh', 'Welsh Family']
+
+    if dorm_name not in mens_dorm and dorm_name not in womens_dorm:
+        return "Invalid dorm name"
+
+    cnx = mysql.connector.connect(user='ktong1', password='pw', host='localhost', database='ktong1')
+    cursor = cnx.cursor()
+
+    query = ("SELECT r.dorm_name, r.floor_num, r.room_num, r.size, r.capacity "
+            "FROM Rooms r, Selections s "
+            "WHERE r.dorm_name = %s and (r.dorm_name <> s.dorm_name or "
+                "r.room_num <> s.room_num)")
+
+    cursor.execute(query, (dorm_name,))
+
+    data = {"rooms": []}
+
+    for i in cursor:
+        room = {}
+        room['dorm_name'] = i[0]
+        room['floor_num'] = i[1]
+        room['room_num'] = i[2]
+        room['size'] = i[3]
+        room['capacity'] = i[4]
+        data['rooms'].append(room)
+
+    cursor.close()
+    cnx.close()
+
+    return json.dumps(data)
+
+@app.route('/filter/<netID>/<dorm_name>/<capacity>/<floor_num>')
+def filter_rooms(netID, dorm_name, capacity, floor_num): #add size filter
+    mens_dorm = ['Alumni', 'Carroll', 'Dillon', 'Duncan', 'Dunne', 'Fisher',
+                 'Keenan', 'Keough', 'Knott', 'Morrisey', 'O\'Neil Family',
+                 'Siegfried', 'Sorin', 'St. Edward\'s', 'Stanford', 'Zahm']
+    womens_dorm = ['Badin', 'Breen-Phillips', 'Cavanaugh', 'Farley', 'Flaherty',
+                   'Howard', 'Lewis', 'Lyons', 'McGlinn', 'Pasquerilla East',
+                   'Pasquerilla West', 'Ryan', 'Walsh', 'Welsh Family']
+
+    if dorm_name not in mens_dorm and dorm_name not in womens_dorm:
+        return "Invalid dorm name"
+
+    cnx = mysql.connector.connect(user='ktong1', password='pw', host='localhost', database='ktong1')
+    cursor = cnx.cursor()
+
+    query = ("SELECT r.dorm_name, r.floor_num, r.room_num, r.size, r.capacity "
+            "FROM Rooms r, Selections s "
+            "WHERE r.dorm_name = %s and (r.dorm_name <> s.dorm_name or "
+                "r.room_num <> s.room_num) and r.capacity = %s and r.floor_num = %s")
+
+    cursor.execute(query, (dorm_name, capacity, floor_num,))
+
+    data = {"rooms": []}
+
+    for i in cursor:
+        room = {}
+        room['dorm_name'] = i[0]
+        room['floor_num'] = i[1]
+        room['room_num'] = i[2]
+        room['size'] = i[3]
+        room['capacity'] = i[4]
+        data['rooms'].append(room)
+
+    cursor.close()
+    cnx.close()
+
+    return json.dumps(data)
+
 @app.route('/rooms/<netID>/<dorm>')
 def query_rooms(netID, dorm):
     # tmp file for dummy json data
