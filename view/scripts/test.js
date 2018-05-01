224888e6145f4cad7d83266b7d30e9c2af04ac75
Alec Borowsky <alecborowsky@alecs-mbp.dhcp.nd.edu>
2018-05-01 15:45:56 -0400

    Fixed room label js

--- view/scripts/test.js
+++ view/scripts/test.js
@@ -23,6 +23,7 @@ function clear_and_populate_all(){
     repopulate_rooms();
     clear_queue();
     get_data(ADDR + ":" + PORT + "/preferences/"+NETID+"/"+DORM_NAME, populate_preference_queue);
+    get_data(ADDR + ":" + PORT + "/selection/", populate_room_pick);
 }
 
 function repopulate_queue(pref_num1, pref_num2){
@@ -705,7 +706,6 @@ function zoom_in_img(imgId){
 
     get_data(ADDR + ":" + PORT + "/time/"+NETID+"/"+DORM_NAME, populate_picktime);
 
-    get_data(ADDR + ":" + PORT + "/selection/", populate_room_pick);
 
 
 
